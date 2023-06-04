bac91acd67b02aaf6b302c44e18d936c06499aea
Syed M.Jafar Ali Naqvi <alimuhammadjafar873@gmail.com>
2023-06-04 13:36:28 +0500

    Few Errors Corrected
    Few functions were not working correctly, so necessary changes are made.

--- script.js
+++ script.js
@@ -1,270 +1,247 @@
 function getter() {
-  var element = document.getElementById("options");
-  var first_type = Number(element.options[element.selectedIndex].value);
-  element = document.getElementById("options2");
-  var second_type = Number(element.options[element.selectedIndex].value);
-  var num = Number(document.getElementById("num_input").value);
-  return [first_type, second_type, num];
+	var element = document.getElementById("options");
+	var first_type = Number(element.options[element.selectedIndex].value);
+	element = document.getElementById("options2");
+	var second_type = Number(element.options[element.selectedIndex].value);
+	var num = Number(document.getElementById("num_input").value);
+	return [first_type, second_type, num];
 }
 function pointPartForBinary(num) {
-  var result = "";
-  arr = [];
-  arr[0] = ".";
-  var limit = 0;
-  while (num != 0.0 && num != 1.0 && !arr.includes(num) && limit < 20) {
-    arr.push(num);
-    num *= 2;
-    result += Math.floor(num);
-    num = num - Math.floor(num);
-    limit++;
-  }
-  return result;
+	var result = "";
+	arr = [];
+	arr[0] = ".";
+	var limit = 0;
+	while (num != 0.0 && num != 1.0 && !arr.includes(num) && limit < 20) {
+		arr.push(num);
+		num *= 2;
+		result += Math.floor(num);
+		num = num - Math.floor(num);
+		limit++;
+	}
+	return result;
 }
 function ZeroCheck(arr) {
-  var result = 1;
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] != 0) {
-      return 0;
-    }
-  }
-  return 1;
+	var result = 1;
+	for (var i = 0; i < arr.length; i++) {
+		if (arr[i] != 0) {
+			return 0;
+		}
+	}
+	return 1;
 }
 function decimalToBinary(num) {
-  var arr = new Array();
-  if (Number.isInteger(num)) {
-    while (num > 0) {
-      if (num % 2 == 0) {
-        arr.push(0);
-      } else if (num % 2 == 1) {
-        arr.push(1);
-        num--;
-      }
-      num = num / 2;
-    }
-    arr.reverse();
-  } else {
-    var integerPart = Math.floor(num);
-    var point = num - integerPart;
-    var real_arr = new Array();
-    var point_arr = new Array();
-    real_arr = decimalToBinary(integerPart);
-    point_arr = pointPartForBinary(point);
+	var arr = [];
 
-    if (ZeroCheck(real_arr)) {
-      new_arr = new Array();
-      new_arr[0] = "0";
-      new_arr[1] = ".";
-      arr = new_arr.concat(point_arr);
-    } else {
-      new_arr = new Array();
-      temp = new Array();
-      temp = new_arr.concat(real_arr);
-      temp.push(".");
-      arr = temp.concat(point_arr);
-    }
-  }
-  return arr;
+	if (Number.isInteger(num)) {
+		while (num > 0) {
+			arr.unshift(num % 2);
+			num = Math.floor(num / 2);
+		}
+	} else {
+		var integerPart = Math.floor(num);
+		var fractionalPart = num - integerPart;
+
+		while (integerPart > 0) {
+			arr.unshift(integerPart % 2);
+			integerPart = Math.floor(integerPart / 2);
+		}
+
+		arr.push("."); // Add the decimal point
+
+		var limit = 20; // Limit the number of fractional digits
+		while (fractionalPart > 0 && limit > 0) {
+			fractionalPart *= 2;
+			arr.push(Math.floor(fractionalPart));
+			fractionalPart -= Math.floor(fractionalPart);
+			limit--;
+		}
+	}
+
+	return arr;
 }
 
 function binaryToDecimal(binary) {
-  let integerPart = binary.split('.')[0];
-  let fractionalPart = binary.split('.')[1] || ''; 
-  let fractionalDecimal = 0;
-  let position = integerPart.length - 1;
-  var integerDecimal=0;
-  for (let i = 0; i < integerPart.length; i++) {
-    integerDecimal += integerPart[i] * Math.pow(2, position);
-    position--;
-  }
-  if (fractionalPart.length > 0) {
-    position = -1;
-    for (let i = 0; i < fractionalPart.length; i++) {
-      fractionalDecimal += fractionalPart[i] * Math.pow(2, position);
-      position--;
-    }
-  }
-  var arr=new Array();
-  arr.push(integerDecimal + fractionalDecimal)
-  return arr;
+	let integerPart = binary.split(".")[0];
+	let fractionalPart = binary.split(".")[1] || "";
+	let fractionalDecimal = 0;
+	let position = integerPart.length - 1;
+	var integerDecimal = 0;
+	for (let i = 0; i < integerPart.length; i++) {
+		integerDecimal += integerPart[i] * Math.pow(2, position);
+		position--;
+	}
+	if (fractionalPart.length > 0) {
+		position = -1;
+		for (let i = 0; i < fractionalPart.length; i++) {
+			fractionalDecimal += fractionalPart[i] * Math.pow(2, position);
+			position--;
+		}
+	}
+	var arr = new Array();
+	arr.push(integerDecimal + fractionalDecimal);
+	return arr;
 }
 
 function decimalToOctal(decimal) {
-  let integerPart = Math.floor(decimal);
-  let fractionalPart = decimal - integerPart;
-  let integerOctal = "";
-  let fractionalOctal = "";
-  
-  while (integerPart > 0) {
-    integerOctal = (integerPart % 8).toString() + integerOctal;
-    integerPart = Math.floor(integerPart / 8);
-  }
-  if (fractionalPart > 0) {
-    fractionalOctal = ".";
-    let precision = 6;
-    while (precision > 0 && fractionalPart > 0) {
-      fractionalPart *= 8;
-      let digit = Math.floor(fractionalPart);
-      fractionalOctal += digit.toString();
-      fractionalPart -= digit;
-      precision--;
-    }
-  }
-  var arr=new Array();
-  arr.push(integerOctal + fractionalOctal);
-  return arr;
+	let integerPart = Math.floor(decimal);
+	let fractionalPart = decimal - integerPart;
+	let integerOctal = "";
+	let fractionalOctal = "";
+
+	while (integerPart > 0) {
+		integerOctal = (integerPart % 8).toString() + integerOctal;
+		integerPart = Math.floor(integerPart / 8);
+	}
+	if (fractionalPart > 0) {
+		fractionalOctal = ".";
+		let precision = 6;
+		while (precision > 0 && fractionalPart > 0) {
+			fractionalPart *= 8;
+			let digit = Math.floor(fractionalPart);
+			fractionalOctal += digit.toString();
+			fractionalPart -= digit;
+			precision--;
+		}
+	}
+	var arr = new Array();
+	arr.push(integerOctal + fractionalOctal);
+	return arr;
 }
 
 function octalToDecimal(octal) {
-  let integerPart = octal.split('.')[0];
-  let fractionalPart = octal.split('.')[1] || ''; 
-  let integerDecimal = 0;
-  let fractionalDecimal = 0;
-  let position = integerPart.length - 1;
-  for (let i = 0; i < integerPart.length; i++) {
-    integerDecimal += integerPart[i] * Math.pow(8, position);
-    position--;
-  }
-  if (fractionalPart.length > 0) { 
-    position = -1;
-    for (let i = 0; i < fractionalPart.length; i++) {
-      fractionalDecimal += fractionalPart[i] * Math.pow(8, position);
-      position--;
-    }
-  }
-  var arr=new Array();
-  arr.push(integerDecimal + fractionalDecimal);
-  return arr;
+	let integerPart = octal.split(".")[0];
+	let fractionalPart = octal.split(".")[1] || "";
+	let integerDecimal = 0;
+	let fractionalDecimal = 0;
+	let position = integerPart.length - 1;
+	for (let i = 0; i < integerPart.length; i++) {
+		integerDecimal += integerPart[i] * Math.pow(8, position);
+		position--;
+	}
+	if (fractionalPart.length > 0) {
+		position = -1;
+		for (let i = 0; i < fractionalPart.length; i++) {
+			fractionalDecimal += fractionalPart[i] * Math.pow(8, position);
+			position--;
+		}
+	}
+	var arr = new Array();
+	arr.push(integerDecimal + fractionalDecimal);
+	return arr;
 }
 function hexToDecimal(hex) {
-  let integerPart = hex.split('.')[0];
-  let fractionalPart = hex.split('.')[1] || ''; 
-  let integerDecimal = 0;
-  let fractionalDecimal = 0;
-  let position = integerPart.length - 1;
-  for (let i = 0; i < integerPart.length; i++) {
-    let digitValue = parseInt(integerPart[i], 16);
-    integerDecimal += digitValue * Math.pow(16, position);
-    position--;
-  }
-  if (fractionalPart.length > 0) { 
-    position = -1;
-    for (let i = 0; i < fractionalPart.length; i++) {
-      let digitValue = parseInt(fractionalPart[i], 16);
-      fractionalDecimal += digitValue * Math.pow(16, position);
-      position--;
-    }
-  }
-  var arr=new Array();
-  arr.push(integerDecimal + fractionalDecimal);
-  return arr;
+	let integerPart = hex.split(".")[0];
+	let fractionalPart = hex.split(".")[1] || "";
+	let integerDecimal = 0;
+	let fractionalDecimal = 0;
+	let position = integerPart.length - 1;
+	for (let i = 0; i < integerPart.length; i++) {
+		let digitValue = parseInt(integerPart[i], 16);
+		integerDecimal += digitValue * Math.pow(16, position);
+		position--;
+	}
+	if (fractionalPart.length > 0) {
+		position = -1;
+		for (let i = 0; i < fractionalPart.length; i++) {
+			let digitValue = parseInt(fractionalPart[i], 16);
+			fractionalDecimal += digitValue * Math.pow(16, position);
+			position--;
+		}
+	}
+	var arr = new Array();
+	arr.push(integerDecimal + fractionalDecimal);
+	return arr;
 }
- 
+
 function decimalToHex(decimal) {
-  let integerPart = Math.floor(decimal);
-  let fractionalPart = decimal - integerPart;
-  let integerHexa = "";
-  let fractionalHexa = "";
-  
-  while (integerPart > 0) {
-    let digitValue = integerPart % 16;
-    if (digitValue >= 10) {
-      integerHexa = String.fromCharCode(65 + digitValue - 10) + integerHexa;
-    } else {
-      integerHexa = digitValue.toString() + integerHexa;
-    }
-    integerPart = Math.floor(integerPart / 16);
-  }
-  if (fractionalPart > 0) {
-    fractionalHexa = ".";
-    let precision = 20;
-    while (precision > 0 && fractionalPart > 0) {
-      fractionalPart *= 16;
-      let digitValue = Math.floor(fractionalPart);
-      if (digitValue >= 10) {
-        fractionalHexa += String.fromCharCode(65 + digitValue - 10);
-      } else {
-        fractionalHexa += digitValue.toString();
-      }
-      fractionalPart -= digitValue;
-      precision--;
-    }
-    if (precision == 0) {
-      fractionalHexa += "A";
-    }
-  }
-  var arr=new Array();
-  arr.push(integerHexa + fractionalHexa);
-  return arr;
-}
+	let integerPart = Math.floor(decimal);
+	let fractionalPart = decimal - integerPart;
+	let integerHexa = "";
+	let fractionalHexa = "";
 
-function binaryToOctal(binary) {
-  // Convert binary to decimal
-  var decimal = binaryToDecimal(binary)[0];
-
-  // Convert decimal to octal
-  var octal = decimalToOctal(decimal)[0];
-
-  // Return octal
-  return octal;
+	while (integerPart > 0) {
+		let digitValue = integerPart % 16;
+		if (digitValue >= 10) {
+			integerHexa = String.fromCharCode(65 + digitValue - 10) + integerHexa;
+		} else {
+			integerHexa = digitValue.toString() + integerHexa;
+		}
+		integerPart = Math.floor(integerPart / 16);
+	}
+	if (fractionalPart > 0) {
+		fractionalHexa = ".";
+		let precision = 20;
+		while (precision > 0 && fractionalPart > 0) {
+			fractionalPart *= 16;
+			let digitValue = Math.floor(fractionalPart);
+			if (digitValue >= 10) {
+				fractionalHexa += String.fromCharCode(65 + digitValue - 10);
+			} else {
+				fractionalHexa += digitValue.toString();
+			}
+			fractionalPart -= digitValue;
+			precision--;
+		}
+		if (precision == 0) {
+			fractionalHexa += "A";
+		}
+	}
+	var arr = new Array();
+	arr.push(integerHexa + fractionalHexa);
+	return arr;
 }
 
 function printResult(result_arr, type) {
-  var result = "";
-  if (type == "binary") {
-    var size = result_arr.length;
-    if (size < 4) {
-      while (size < 4) {
-        result += "0";
-        size++;
-      }
-    }
-  }
+	var result = "";
+	if (type === "binary") {
+		var size = result_arr.length;
+		if (size < 4) {
+			while (size < 4) {
+				result += "0";
+				size++;
+			}
+		}
+	}
 
-  for (var i = 0; i < result_arr.length; i++) {
-    result += result_arr[i];
-  }
-  if (result.length > 15) {
-    document.getElementById("answer").style.paddingRight = "0rem";
-  }
+	for (var i = 0; i < result_arr.length; i++) {
+		result += result_arr[i];
+	}
 
-  document.getElementById("answer").innerHTML = result_arr[0];
+	if (result.length > 15) {
+		document.getElementById("answer").style.paddingRight = "0rem";
+	}
+
+	document.getElementById("answer").innerHTML = result;
 }
+
 function computer() {
-  var first_type = 0;
-  var second_type = 0;
-  var number = 0;
-  var arr = getter();
-  first_type = arr[0];
-  second_type = arr[1];
-  number = arr[2];
-  var result_arr = [];
-  var type = "";
-  if (first_type == 10 && second_type == 2) {
-    result_arr = decimalToBinary(number);
-    type = "binary";
-  } else if (first_type == 2 && second_type == 10) {
-    result_arr = binaryToDecimal(number.toString());
-    type = "decimal";
-  }
-  else if (first_type == 10 && second_type == 8) {
-    result_arr = decimalToOctal(number);
-    type = "octal";
-  }
-  else if (first_type == 8 && second_type == 10) {
-    result_arr = octalToDecimal(number.toString());
-    type = "octal";
-  }
-  else if (first_type == 10 && second_type == 16) {
-    result_arr = decimalToHex(number);
-    type = "hexa";
-  }
-  else if (first_type == 16 && second_type == 10) {
-    result_arr = hexToDecimal(number.toString());
-    type = "hexa";
-  }
-  else if (first_type == 2 && second_type == 8) {
-    result_arr = binaryToOctal(number.toString());
-    type = "octal";
-  }
-  printResult(result_arr, type);
+	var first_type = 0;
+	var second_type = 0;
+	var number = 0;
+	var arr = getter();
+	first_type = arr[0];
+	second_type = arr[1];
+	number = arr[2];
+	var result_arr = [];
+	var type = "";
+	if (first_type == 10 && second_type == 2) {
+		result_arr = decimalToBinary(number);
+		type = "binary";
+	} else if (first_type == 2 && second_type == 10) {
+		result_arr = binaryToDecimal(number.toString());
+		type = "decimal";
+	} else if (first_type == 10 && second_type == 8) {
+		result_arr = decimalToOctal(number);
+		type = "octal";
+	} else if (first_type == 8 && second_type == 10) {
+		result_arr = octalToDecimal(number.toString());
+		type = "octal";
+	} else if (first_type == 10 && second_type == 16) {
+		result_arr = decimalToHex(number);
+		type = "hexa";
+	} else if (first_type == 16 && second_type == 10) {
+		result_arr = hexToDecimal(number.toString());
+		type = "hexa";
+	}
+	printResult(result_arr, type);
 }
